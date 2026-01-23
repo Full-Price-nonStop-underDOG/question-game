43ed65b671483a9f9e01d7a60b53b1f46ab1bd65
full-price-nonstop-underdog <korvin.korp@yandex.ru>
2026-01-23 18:36:28 +0300

    added lobby screen plus micro navigation

--- main.js
+++ main.js
@@ -49,6 +49,14 @@ if (joinGameBtn) {
   console.log("Join game button clicked");
 }
 
+const toLobbyBtn = document.getElementById("toLobbyBtn");
+if (toLobbyBtn) {
+    toLobbyBtn.addEventListener("click", () => {
+    window.location.href = "lobby.html";
+  });
+  console.log("Join game");
+}
+
 
 
 const backBtn = document.getElementById("backBtn");
